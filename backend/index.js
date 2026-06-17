41047762f836e1f13cfb34ea4630641a2589fad9
Zacar <sakarmanandhar1@gmail.com>
2026-06-17 15:30:32 +0545

    successful get update delete routes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ app.use(express.json()); // req.body
 
 app.post("/mini_tracker", async (req, res) => {
   try {
-    //Destructure the values sent from Postman's JSON body
     const { company_name, job_title, job_type, status, applied_date, notes } =
       req.body;
 
@@ -21,9 +20,9 @@ app.post("/mini_tracker", async (req, res) => {
         company_name,
         job_title,
         job_type,
-        status || "Applied", // Falls back to 'Applied' if status is missing
-        applied_date || new Date(), // Falls back to today's date if missing
-        notes || "", // Falls back to empty string if missing
+        status || "Applied",
+        applied_date || new Date(),
+        notes || "",
       ]
     );
 
@@ -39,6 +38,83 @@ app.post("/mini_tracker", async (req, res) => {
   } catch (err) {
     console.error("Database Insert Error:", err.message);
     res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/mini_tracker", async (req, res) => {
+  try {
+    const allApplication = await pool.query("SELECT * FROM applications");
+    res.json(allApplication.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//so we can get applications with required id
+app.get("/mini_tracker/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const application = await pool.query(
+      "SELECT * FROM applications WHERE id = $1",
+      [id]
+    );
+    res.json(application.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//for update
+app.put("/mini_tracker/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { company_name, job_title, job_type, status, applied_date, notes } =
+      req.body;
+    const updateApplication = await pool.query(
+      `UPDATE applications 
+        SET company_name = COALESCE($1, company_name), 
+           job_title    = COALESCE($2, job_title), 
+           job_type     = COALESCE($3, job_type), 
+           status       = COALESCE($4, status), 
+           applied_date = COALESCE($5, applied_date), 
+           notes        = COALESCE($6, notes),
+             updated_at = NOW()
+         WHERE id = $7 
+         RETURNING *`,
+      [company_name, job_title, job_type, status, applied_date, notes, id]
+    );
+
+    //to check if data with id exists
+    if (updateApplication.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Application with ID ${id} was not found.`,
+      });
+    }
+
+    //  updated data log
+    console.log(updateApplication.rows[0]);
+
+    res.json({
+      success: true,
+      message: "Application updated successfully!",
+      data: updateApplication.rows[0],
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.delete("/mini_tracker/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteApplication = await pool.query(
+      "DELETE FROM  applications WHERE id=$1",
+      [id]
+    );
+    res.json("application was deleted");
+  } catch (err) {
+    console.log(err.message);
   }
 });
 
